3c4ad3edd86231ff20c5faa96478888e5c53ad9c
Guilherme <guiga.arruda@hotmail.com>
2024-04-01 00:16:05 +0200

    added test folder and categories table creation with alembic

---
+++ app/test/schemas/test_category_schema.py
@@ -0,0 +1,31 @@
+import pytest
+from app.schemas.category import Category
+
+def test_category_schema():
+    category = Category(
+        name='Roupa',
+        slug='roupa'
+    )
+    
+    assert category.dict() == {
+        'name':'Roupa',
+        'slug':'roupa'
+    }
+    
+def test_category_schema_invalid_slug():
+    with pytest.raises(ValueError):
+        category = Category(
+            name='Roupa',
+            slug='roupa de cama'
+        )
+    with pytest.raises(ValueError): #special caracters
+        category = Category(
+            name='Roupa',
+            slug='Ção'
+        )
+    with pytest.raises(ValueError):   #capital letter
+        category = Category(
+            name='Roupa',
+            slug='Roupa'
+        )    
+    
